baedbd0a3622b6ad80d3421b436c5a24c69eea04
Jahnavi Mandapalli <mandapallijahnavi957@gmail.com>
2023-08-23 05:55:31 +0000

    Success

--- app.py
+++ app.py
@@ -55,11 +55,39 @@ async def upload_image(request: Request,image:Annotated[UploadFile, File(...)]):
     # predicted_image = Image.fromarray(integer_rgb_array, mode = "RGB")
     predicted_image = Image.fromarray(integer_rgb_array)
 
-
     byte_data=io.BytesIO()
     predicted_image.save(byte_data,format= 'PNG')
     byte_data=byte_data.getvalue()
     encoded_img=base64.b64encode(byte_data).decode('utf-8')
     # print(predictions)
-    return templates.TemplateResponse("index.html", {"request": request, "img":encoded_img})
+
+    second_model_path = "Swan_resol_3780.h5"  # Replace with the actual second model's path
+    blob = bucket.blob(second_model_path)
+    blob.download_to_filename(second_model_path)
+
+    second_model = tf.keras.models.load_model(second_model_path)
+
+    # Convert the encoded image back to bytes and process it
+    decoded_img = base64.b64decode(encoded_img)
+    decoded_img = io.BytesIO(decoded_img)
+    input_image = Image.open(decoded_img)
+    input_image = input_image.resize((256, 256))
+    input_array = np.array(input_image) / 255
+    input_array = np.expand_dims(input_array, axis=0)
+
+    second_predictions = second_model.predict(input_array)
+    os.remove(second_model_path)
+
+    second_predictions = np.reshape(second_predictions, (256, 256, 3))
+    second_predictions = (second_predictions) / 2.0
+    integer_rgb_array = np.uint8((second_predictions + 0.5) * 255)
+    # predicted_image = Image.fromarray(integer_rgb_array, mode = "RGB")
+    second_output_image = Image.fromarray(integer_rgb_array)
+
+    second_output_byte_data=io.BytesIO()
+    second_output_image.save(second_output_byte_data,format= 'PNG')
+    second_output_byte_data=second_output_byte_data.getvalue()
+    encoded_second_output_img=base64.b64encode(second_output_byte_data).decode('utf-8')
+
+    return templates.TemplateResponse("index.html", {"request": request, "second_output_img": encoded_second_output_img})
 
